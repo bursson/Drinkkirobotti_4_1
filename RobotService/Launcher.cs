f85137abed93333cafc1460f65b476adbf24329c
Ville Lepistö <ville.lepisto@student.tut.fi>
2017-02-10 18:30:29 +0200

    Added NLog and some initial specifications.

---
+++ RobotService/Launcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace RobotService
+{
+    class Launcher
+    {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            const string funcName = nameof(Main);
+            // Use the following notation to save compilation time incase logmessage is not written. (nlog tutorial). 
+            log.Debug("{0}Launching business logic", funcName);
+        }
+    }
+}
