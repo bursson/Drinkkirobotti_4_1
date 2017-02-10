c60043c4ad3bb57e7da1183d931314885d10c4de
Ville Lepistö <ville.lepisto@student.tut.fi>
2017-02-10 19:16:15 +0200

    Added NLogExtensions to the Common-project.

---
+++ Common/NLogExtensions.cs
@@ -0,0 +1,34 @@
+﻿using NLog;
+
+namespace Common
+{
+    /// <summary>
+    /// Public static class to extend functionality of the NLog-library.
+    /// Simplifies the calling syntax and enables the implemenation for TCP logging in the future.
+    /// </summary>
+    public static class NLogExtensions
+    { 
+        public static void TraceEx(this Logger log, string funcName, string message)
+        {
+            log.Trace("{0}|{1}",funcName,message);
+        }
+        public static void DebugEx(this Logger log, string funcName, string message)
+        {
+            log.Debug("{0}|{1}",funcName,message);
+        }
+
+        public static void ErrorEx(this Logger log, string funcName, string message)
+        {
+            log.Error("{0}|{1}", funcName, message);
+        }
+
+        public static void FatalEx(this Logger log, string funcName, string message)
+        {
+            log.Fatal("{0}|{1}", funcName, message);
+        }
+        public static void InfoEx(this Logger log, string funcName, string message)
+        {
+            log.Info("{0}|{1}", funcName, message);
+        }
+    }
+}
--- RobotService/Launcher.cs
+++ RobotService/Launcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 using NLog;
 
 namespace RobotService
@@ -12,9 +13,8 @@ namespace RobotService
         private static Logger log = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            const string funcName = nameof(Main);
-            // Use the following notation to save compilation time incase logmessage is not written. (nlog tutorial). 
-            log.Debug("{0}Launching business logic", funcName);
+            const string funcName = nameof(Main); 
+            log.InfoEx(funcName,"Launching business logic..");
         }
     }
 }
