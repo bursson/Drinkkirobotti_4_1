a034ec5e08708351e8ba039bef7dcab1964283c2
Oskari Burri <oskari.burri@student.tut.fi>
2017-02-23 19:29:05 +0200

    Added required classes and their unit tests

---
+++ RobotService/LogicClasses.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService
+{
+    public struct Bottle
+    {
+        public string Name;
+
+        public Bottle(string bottleName)
+        {
+            Name = bottleName;
+        }
+    }
+
+    public struct Portion
+    {
+        public Bottle Bottle;
+        public int Amount;
+
+        public Portion(Bottle bottle, int amount)
+        {
+            this.Bottle = bottle;
+            this.Amount = amount;
+        }
+
+        /// <summary>
+        /// Check if the portion is valid
+        /// </summary>
+        /// <returns>True if the bottle has a name and amount is over zero</returns>
+        public bool IsValid()
+        {
+            return  Bottle.Name.Length > 0 && Amount > 0;
+        }
+    }
+    public class Drink
+    {
+        private List<Portion> _portions;
+        public string Name { get; set; }
+
+        public Drink(string name)
+        {
+            Name = name;
+            _portions = new List<Portion>();
+        }
+        /// <summary>
+        /// Add a postion to the drink
+        /// </summary>
+        /// <param name="portion">The portion</param>
+        /// <returns>Returns true if a new portion was added or edited</returns>
+        public bool AddPortion(Portion portion)
+        {
+            for (var i = 0; i < _portions.Count; ++i)
+            {
+                if (_portions[i].Bottle.Name != portion.Bottle.Name) continue;
+                if (_portions[i].Amount == portion.Amount)
+                {
+                    return false;
+                }
+                _portions[i] = portion;
+                return true;
+            }
+
+            _portions.Add(portion);
+            return true;
+        }
+
+        /// <summary>
+        /// Insert a new portion to the drink
+        /// </summary>
+        /// <param name="bottle">The bottle</param>
+        /// <param name="amount">The amoount in cl</param>
+        /// <returns>Returns true if a new portion was added or edited</returns>
+        public bool AddPortion(Bottle bottle, int amount)
+        {
+            return AddPortion(new Portion(bottle, amount));
+        }
+
+        public List<Portion> Portions() { return _portions;}
+    }
+    public class Order
+    {
+        private static OrderType _orderType;
+        private static int _id;
+        private static Drink _drink;
+
+        /// <summary>
+        /// Create new order
+        /// </summary>
+        /// <param name="orderType">Type of the order</param>
+        /// <param name="id">Unique id for the order</param>
+        /// <param name="drink">If the order is a drink, pass it here</param>
+        public Order(OrderType orderType, int id, Drink drink = null)
+        {
+            _orderType = orderType;
+            _id = id;
+            _drink = drink;
+        }
+        
+        public static int GetId()
+        {
+            return _id;
+        }
+
+        public static OrderType GetOrderType()
+        {
+            return _orderType;
+        }
+
+        public static Drink GetRecipe()
+        {
+            return _drink;
+        }
+
+    }
+}
---
+++ UnitTests/Classes.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RobotService;
+
+
+namespace RobotService
+{
+    [TestClass]
+    [SuppressMessage("ReSharper", "SuggestVarOrType_SimpleTypes")]
+    public class UnitTest1
+    {
+        private List<Bottle> _bottleshelf;
+        private List<Drink> _drinkdb;
+        [TestInitialize]
+        public void InitClasses()
+        {
+            _bottleshelf = new List<Bottle>(); //TODO: replace with actual bottleshelf
+            _drinkdb = new List<Drink>();
+
+        }
+
+        [TestMethod]
+        public void TestPortion()
+        {
+            Bottle justABottle = new Bottle("something");
+            Portion invalidPortion = new Portion(justABottle, 0);
+            Assert.IsFalse(invalidPortion.IsValid());
+            invalidPortion.Bottle.Name = "";
+            invalidPortion.Amount = 10;
+            Assert.IsFalse(invalidPortion.IsValid());
+
+            Portion validPortion = new Portion(justABottle, 2);
+            Assert.IsTrue(validPortion.IsValid());
+
+
+        }
+        [TestMethod]
+        public void TestOrder()
+        {
+            var justADrink = new Drink("some");
+            var justAOrder = new Order(OrderType.Drink, 1, justADrink);
+            Assert.AreEqual(Order.GetId(), 1, "OrderID");
+            Assert.AreEqual(Order.GetOrderType(), OrderType.Drink, "Ordertype");
+            justADrink.AddPortion(new Portion(new Bottle("a"), 3));
+            justAOrder = new Order(OrderType.Drink, 2, justADrink);
+            Assert.AreEqual(Order.GetRecipe(), justADrink, "Getrecipe");
+
+
+        }
+        [TestMethod]
+        public void TestClasses()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _bottleshelf.Add(new Bottle("Bottle" + i));
+                Assert.AreEqual(_bottleshelf.Count, i + 1, "Bottle" + i);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                _drinkdb.Add(new Drink("Testdrink" + i));
+                for (var index = 0; index < _bottleshelf.Count; index++)
+                {
+                    if (index < 3)
+                    {
+                        Bottle t = _bottleshelf[index];
+                        _drinkdb[i].AddPortion(t, i);
+                    }
+                    else
+                    {
+                        Portion newPortion = new Portion(_bottleshelf[index], i);
+                        _drinkdb[i].AddPortion(newPortion);
+                    }
+
+                }
+            }
+            for (var i = 0; i < _bottleshelf.Count; ++i)
+            {
+                Assert.AreEqual(_drinkdb[i].Name, "Testdrink" + i, $"Drinkname {_drinkdb[i].Name}");
+                for (int j = 0; j < _bottleshelf.Count; j++)
+                {
+                    Assert.AreEqual(_drinkdb[i].Portions()[j].Bottle, _bottleshelf[j], $"Bottlename {_bottleshelf[j].Name}");
+                    Assert.AreEqual(_drinkdb[i].Portions()[j].Amount, i, $"BottleAmount {_bottleshelf[j].Name}");
+                }
+
+
+            }
+
+        }
+    }
+}
