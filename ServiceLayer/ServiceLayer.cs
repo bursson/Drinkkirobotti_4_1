3f6dcd6864a20cf18e8a682a66b9a63231f5c477
linnatee <linnatee@TREKT76>
2017-03-10 19:37:55 +0200

    ServiceLayer interface and messagetypes sketch

--- ServiceLayer/ServiceLayer.cs
+++ ServiceLayer/ServiceLayer.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServiceLayer
 {
     public class ServiceLayer
     {
+        public async Task<Data> ReadDataAsync(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Config> ReadConfigAsync(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> WriteAsync(CancellationToken ct, Data data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> WriteAsync(CancellationToken ct, Config config)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
