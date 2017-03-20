c9a8d92e51f15f8d91e772754ae817f10ae4d733
Oskari Burri <oskari.burri@student.tut.fi>
2017-03-20 13:16:40 +0200

    Added RobotCellLayer implementation

--- RobotCellLayer/RobotCellLayer.cs
+++ RobotCellLayer/RobotCellLayer.cs
@@ -8,5 +8,67 @@ namespace RobotCellLayer
 {
     public class RobotCellLayer
     {
+        private IDeviceComm _robot;
+
+        public RobotCellLayer()
+        {
+            _robot = null;
+        }
+        public bool AddRobot(string name, string portname)
+        {
+            if (portname.Contains("COM"))
+            {
+                try
+                {
+                    _robot = new SerialComm(portname, 0);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+        public async Task grabBottle(int position, int bottletype = 0)
+        {
+            if (_robot == null)
+            {
+                throw new NullReferenceException("Robot is not defined");
+            }
+            var result = _robot.SendCommand($"grabBottle({position},{bottletype})");
+        }
+        public async Task returnBottle(int position, int bottletype = 0)
+        {
+            if (_robot == null)
+            {
+                throw new NullReferenceException("Robot is not defined");
+            }
+            var result = _robot.SendCommand($"returnBottle({position},{bottletype})");
+        }
+        public async Task getNewBottle(int position, int bottletype = 0)
+        {
+            if (_robot == null)
+            {
+                throw new NullReferenceException("Robot is not defined");
+            }
+            var result = _robot.SendCommand($"getNewBottle({position},{bottletype})");
+        }
+        public async Task removeBottle(int bottletype = 0)
+        {
+            if (_robot == null)
+            {
+                throw new NullReferenceException("Robot is not defined");
+            }
+            var result = _robot.SendCommand($"removeBottle({bottletype})");
+        }
+        public async Task pourBottle(int amount, int howMany, int bottletype = 0)
+        {
+            if (_robot == null)
+            {
+                throw new NullReferenceException("Robot is not defined");
+            }
+            var result = _robot.SendCommand($"pourDrinks({amount},{howMany},{bottletype})");
+        }
     }
 }
